2f4c2a950c484a0cb6deffbe3de6d135043b3666
Azzakiel <berneyael.contact@gmail.com>
2017-06-08 23:19:19 +0200

    test

---
+++ general_loader.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: berne
+ * Date: 05/06/2017
+ * Time: 15:23
+ */
+
+session_start();
+include_once 'pkg_mysql/bdd.php';
+include_once  'pkg_html/html_loader.php';
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: berne
+ * Date: 08/06/2017
+ * Time: 23:11
+ */
